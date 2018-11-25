7bd001174e4e359bcf22f4af28f6e2260b3bcf10
JiahuiYu <JiahuiYu@users.noreply.github.com>
2018-11-25 10:27:45 -0600

    Remove explicit padding

--- wdsr_a.py
+++ wdsr_a.py
@@ -42,7 +42,7 @@ class MODEL(nn.Module):
         # define head module
         head = []
         head.append(
-            wn(nn.Conv2d(args.n_colors, n_feats, 3, padding=0//2)))
+            wn(nn.Conv2d(args.n_colors, n_feats, 3, padding=3//2)))
 
         # define body module
         body = []
@@ -54,34 +54,26 @@ class MODEL(nn.Module):
         tail = []
         out_feats = scale*scale*args.n_colors
         tail.append(
-            wn(nn.Conv2d(n_feats, out_feats, 3, padding=0//2)))
+            wn(nn.Conv2d(n_feats, out_feats, 3, padding=3//2)))
         tail.append(nn.PixelShuffle(scale))
 
         skip = []
         skip.append(
-            wn(nn.Conv2d(args.n_colors, out_feats, 5, padding=0//2))
+            wn(nn.Conv2d(args.n_colors, out_feats, 5, padding=5//2))
         )
         skip.append(nn.PixelShuffle(scale))
-
-        pad = []
-        pad.append(torch.nn.ReplicationPad2d(5//2))
 
         # make object members
         self.head = nn.Sequential(*head)
         self.body = nn.Sequential(*body)
         self.tail = nn.Sequential(*tail)
         self.skip = nn.Sequential(*skip)
-        self.pad = nn.Sequential(*pad)
 
     def forward(self, x):
         x = (x - self.rgb_mean.cuda()*255)/127.5
-        if not self.training:
-            x = self.pad(x)
         s = self.skip(x)
         x = self.head(x)
-
         x = self.body(x)
-
         x = self.tail(x)
         x += s
         x = x*127.5 + self.rgb_mean.cuda()*255
